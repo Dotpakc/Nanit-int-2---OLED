6213dfa886466a4f5fd9c9b41f8c14888428ad7f
DotPack <lifirenko123@gmail.com>
2023-11-18 22:59:44 +0200

    Add new libraries for ArduinoMenu and TimerOne

--- src/main.cpp
+++ src/main.cpp
@@ -2,152 +2,261 @@
 #include <U8g2lib.h>
 #include <EncButton.h>
 
-// U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 22, /* data=*/ 21);   // ESP32 Thing, HW I2C with pin remapping
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 22, /* data=*/ 21);   // ESP32 Thing, HW I2C with pin remapping
-// 
+#include <menu.h>
+#include <menuIO/u8g2Out.h>
+#include <menuIO/encoderIn.h>
+#include <menuIO/keyIn.h>
+#include <menuIO/interruptPins.h>
+#include <menuIO/chainStream.h>
+#include <menuIO/serialOut.h>
+#include <menuIO/serialIn.h>
+#include <Wire.h>
 
-EncButton ecB(5, 18, 19);
+
+using namespace Menu;
+
+#define LEDPIN LED_BUILTIN
+
+#define fontName u8g2_font_5x7_tf
+#define fontX 6
+#define fontY 9
+#define offsetX 0
+#define offsetY 0
+#define U8_Width 128
+#define U8_Height 32
+#define USE_HWI2C
+
+#define encA    5
+#define encB    18
+#define encBtn  19
 
 
-void isr() {
-  ecB.tickISR();
+
+
+EncButton enc(encA, encB, encBtn);
+U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R2, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);   // pin remapping with ESP8266 HW I2C
+
+IRAM_ATTR void isr() {
+  enc.tickISR();
 }
 
-#define COUNT(x)  sizeof(x)/sizeof(*x)
+IRAM_ATTR void isrBtn() {
+  enc.tick();
+}
 
+// define menu colors --------------------------------------------------------
+//each color is in the format:
+//  {{disabled normal,disabled selected},{enabled normal,enabled selected, enabled editing}}
+// this is a monochromatic color table
+const colorDef<uint8_t> colors[6] MEMMODE={
+  {{0,0},{0,1,1}},//bgColor
+  {{1,1},{1,0,0}},//fgColor
+  {{1,1},{1,0,0}},//valColor
+  {{1,1},{1,0,0}},//unitColor
+  {{0,1},{0,0,1}},//cursorColor
+  {{1,1},{1,0,0}},//titleColor
+};
+
+result doAlert(eventMask e, prompt &item);
+
+int test=55;
+
+int ledCtrl=HIGH;
+
+result myLedOn() {
+  ledCtrl=HIGH;
+  return proceed;
+}
+result myLedOff() {
+  ledCtrl=LOW;
+  return proceed;
+}
+
+TOGGLE(ledCtrl,setLed,"Led: ",doNothing,noEvent,noStyle//,doExit,enterEvent,noStyle
+  ,VALUE("On",HIGH,doNothing,noEvent)
+  ,VALUE("Off",LOW,doNothing,noEvent)
+);
+
+int selTest=0;
+SELECT(selTest,selMenu,"Select",doNothing,noEvent,noStyle
+  ,VALUE("Zero",0,doNothing,noEvent)
+  ,VALUE("One",1,doNothing,noEvent)
+  ,VALUE("Two",2,doNothing,noEvent)
+);
+
+int chooseTest=-1;
+CHOOSE(chooseTest,chooseMenu,"Choose",doNothing,noEvent,noStyle
+  ,VALUE("First",1,doNothing,noEvent)
+  ,VALUE("Second",2,doNothing,noEvent)
+  ,VALUE("Third",3,doNothing,noEvent)
+  ,VALUE("Last",-1,doNothing,noEvent)
+);
+
+// //customizing a prompt look!
+// //by extending the prompt class
+// class altPrompt:public prompt {
+// public:
+//   altPrompt(constMEM promptShadow& p):prompt(p) {}
+//   Used printTo(navRoot &root,bool sel,menuOut& out, idx_t idx,idx_t len,idx_t panelNr) override {
+//     return out.printRaw(F("special prompt!"),len);;
+//   }
+// };
+
+MENU(subMenu,"Sub-Menu",doNothing,noEvent,noStyle
+  ,OP("Sub1",doNothing,noEvent)
+  // ,altOP(altPrompt,"",doNothing,noEvent)
+  ,EXIT("<Back")
+);
+
+uint16_t hrs=0;
+uint16_t mins=0;
+
+//define a pad style menu (single line menu)
+//here with a set of fields to enter a date in YYYY/MM/DD format
+altMENU(menu,timeMenu,"Time",doNothing,noEvent,noStyle,(systemStyles)(_asPad|Menu::_menuData|Menu::_canNav|_parentDraw)
+  ,FIELD(hrs,"",":",0,11,1,0,doNothing,noEvent,noStyle)
+  ,FIELD(mins,"","",0,59,10,1,doNothing,noEvent,wrapStyle)
+);
+
+char* constMEM hexDigit MEMMODE="0123456789ABCDEF";
+char* constMEM hexNr[] MEMMODE={"0","x",hexDigit,hexDigit};
+char buf1[]="0x11";
+
+int timeOn=10;
+int timeOff=90;
+
+
+MENU(mainMenu,"Main menu",doNothing,noEvent,wrapStyle
+  ,FIELD(timeOn,"On","ms",0,100,10,1,doNothing,noEvent,noStyle)
+  ,FIELD(timeOff,"Off","ms",0,100,10,1,doNothing,noEvent,noStyle)
+  ,OP("Alert",doAlert, enterEvent)
+);
+
+#define MAX_DEPTH 2
+
+// encoderIn<encA,encB> encoder;//simple quad encoder driver
+// encoderInStream<encA,encB> encStream(encoder,4);// simple quad encoder fake Stream
+
+//a keyboard with only one key as the encoder button
+// keyMap encBtn_map[]={{-encBtn,defaultNavCodes[enterCmd].ch}};//negative pin numbers use internal pull-up, this is on when low
+// keyIn<1> encButton(encBtn_map);//1 is the number of keys
+
+// menuIn* inputsList[]={&encBuitton,&Serial};
+// chainStream<2> in(inputsList);//1 is the number of inputs
+
+serialIn serial(Serial);
+MENU_INPUTS(in,&serial);
+// MENU_INPUTS(in,&encStream,&encButton);//,&serial);
+
+MENU_OUTPUTS(out,MAX_DEPTH
+  ,U8G2_OUT(u8g2,colors,fontX,fontY,offsetX,offsetY,{0,0,U8_Width/fontX,U8_Height/fontY})
+  ,SERIAL_OUT(Serial)
+);
+
+NAVROOT(nav,mainMenu,MAX_DEPTH,in,out);
+
+result alert(menuOut& o,idleEvent e) {
+  if (e==idling) {
+    o.setCursor(0,0);
+    o.print("alert test");
+    o.setCursor(0,1);
+    o.print("press [select]");
+    o.setCursor(0,2);
+    o.print("to continue...");
+    
+
+  }
+  return proceed;
+}
+
+result doAlert(eventMask e, prompt &item) {
+  nav.idleOn(alert);
+  do{
+  digitalWrite(LEDPIN, HIGH);
+  delay(timeOn);
+  digitalWrite(LEDPIN, LOW);
+  delay(timeOff);
+  enc.tick();
+  if(enc.press()){
+    nav.idleOff();
+    return proceed;}
+  }while(true);
+  // return proceed;
+}
+
+//when menu is suspended
+result idle(menuOut& o,idleEvent e) {
+  o.clear();
+  switch(e) {
+    case idleStart:o.println("suspending menu!");break;
+    case idling:o.println("suspended...");break;
+    case idleEnd:o.println("resuming menu.");break;
+  }
+  return proceed;
+}
 
 void setup() {
-  u8g2.begin();
+  pinMode(LEDPIN,OUTPUT);
+  attachInterrupt(encA, isr, CHANGE);
+  attachInterrupt(encB, isr, CHANGE);
+  enc.setEncISR(true);
 
-  attachInterrupt(5, isr, CHANGE);
-  attachInterrupt(18, isr, CHANGE);
-  ecB.setEncISR(true);
-  
-  u8g2.setFont(u8g2_font_ncenB08_tr);
-  u8g2.drawStr(0,10,"Hello World!");
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("menu 4.x test");Serial.flush();
+  // encButton.begin();
+  // encoder.begin();
+  // pinMode(LEDPIN,OUTPUT);//cant use pin 13 when using hw spi
+  // and on esp12 i2c can be on pin 2, and that is also led pin
+  // so check first if this is adequate for your board
+  #if defined(USE_HWSPI)
+    SPI.begin();
+    u8g2.begin();
+  #elif defined(USE_HWI2C)
+    Wire.begin();
+    u8g2.begin();
+  #else
+    #error "please choose your interface (I2c,SPI)"
+  #endif
+  u8g2.setFont(fontName);
+  // u8g2.setBitmapMode(0);
 
-  u8g2.sendBuffer();
   
+
+  // disable second option
+  // mainMenu[1].enabled=disabledStatus;
+  nav.idleTask=idle;//point a function to be used when menu is suspended
+  Serial.println("setup done.");Serial.flush();
 }
-
-const char *txtMENU[] = {
-    "LED",
-    "BUZZER",
-    "OLED",
-    "BUTTON",
-    "POTENTIOMETER",
-    "LIGHT",
-    "SOUND",
-    "TEMP. & HUMIDITY",
-    "AIR PRESSURE",
-    "3-AXIS ACCELEMETER"
-};
-
-const char *dscMENU[] = {
-    "BLINKING LED",
-    "PLAYING GAME MELODIES",
-    "SHOWING A 3D CUBE EFFECT",
-    "COUNTING BUTTON PRESSES",
-    "MODIFYING CONTRAST LEVEL",
-    "",
-    "",
-    "",
-    "",
-    "MOVING A BALL BY YOURSELF"
-};
-
-const byte itemsMENU = COUNT(txtMENU);
-
-const unsigned char xMenuUp[]     U8X8_PROGMEM = { /*16x16px*/ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xc0, 0x00, 0xe0, 0x01, 0xf0, 0x03, 0x38, 0x07, 0x1c, 0x0e, 0x0e, 0x1c, 0x07, 0x38, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-const unsigned char xMenuDown[]   U8X8_PROGMEM = { /*16x16px*/ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07, 0x38, 0x0e, 0x1c, 0x1c, 0x0e, 0x38, 0x07, 0xf0, 0x03, 0xe0, 0x01, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-const unsigned char xMenuLeft[]   U8X8_PROGMEM = { /*10x10px*/ 0xe0, 0x00, 0x70, 0x00, 0x38, 0x00, 0x1c, 0x00, 0x0e, 0x00, 0x0e, 0x00, 0x1c, 0x00, 0x38, 0x00, 0x70, 0x00, 0xe0, 0x00 };
-const unsigned char xMenuRight[]  U8X8_PROGMEM = { /*10x10px*/ 0x1c, 0x00, 0x38, 0x00, 0x70, 0x00, 0xe0, 0x00, 0xc0, 0x01, 0xc0, 0x01, 0xe0, 0x00, 0x70, 0x00, 0x38, 0x00, 0x1c, 0x00 };
-
-
-
-const unsigned char xbmMENU[itemsMENU][32] U8X8_PROGMEM = //16x16px
-{
-    { 0x00, 0x00, 0xe0, 0x01, 0xe0, 0x01, 0x60, 0x3f, 0x7f, 0x7f, 0x7f, 0xc1, 0x60, 0x81, 0x60, 0x8f, 0x60, 0x8f, 0x60, 0x81, 0x7f, 0xc1, 0x7f, 0x7f, 0x60, 0x3f, 0xe0, 0x01, 0xe0, 0x01, 0x00, 0x00 },
-    { 0x00, 0x00, 0x80, 0x01, 0xc0, 0x01, 0xe0, 0x01, 0xb0, 0x11, 0x9e, 0x21, 0x8e, 0x45, 0x86, 0x49, 0x86, 0x49, 0x8e, 0x45, 0x9e, 0x21, 0xb0, 0x11, 0xe0, 0x01, 0xc0, 0x01, 0x80, 0x01, 0x00, 0x00 },
-    { 0x00, 0x00, 0x00, 0x00, 0xfe, 0x7f, 0xfe, 0x7f, 0x06, 0x60, 0x06, 0x60, 0x06, 0x60, 0x06, 0x60, 0x06, 0x60, 0xfe, 0x7f, 0xfe, 0x7f, 0x80, 0x01, 0x80, 0x01, 0xf0, 0x0f, 0xf0, 0x0f, 0x00, 0x00 },
-    { 0x00, 0x00, 0xe0, 0x07, 0xe0, 0x07, 0xe0, 0x07, 0xe0, 0x07, 0xfe, 0x7f, 0xfe, 0x7f, 0x06, 0x60, 0x06, 0x60, 0x36, 0x6c, 0xfe, 0x7f, 0xfe, 0x7f, 0x30, 0x0c, 0x30, 0x0c, 0x30, 0x0c, 0x00, 0x00 },
-    { 0x80, 0x01, 0xc0, 0x03, 0xc0, 0x03, 0xc0, 0x03, 0xc0, 0x03, 0xc0, 0x03, 0xfe, 0x7f, 0xff, 0xff, 0x03, 0xc0, 0x03, 0xc0, 0x9b, 0xd9, 0xff, 0xff, 0xfe, 0x7f, 0x98, 0x19, 0x98, 0x19, 0x98, 0x19 },
-    { 0x00, 0x00, 0x80, 0x01, 0xc0, 0x03, 0xc0, 0x03, 0xc0, 0x03, 0x60, 0x06, 0x30, 0x0c, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x1a, 0x58, 0x31, 0x8c, 0xe4, 0x27, 0xc2, 0x43, 0x10, 0x08, 0x08, 0x10 },
-    { 0x00, 0x00, 0xc0, 0x03, 0xe0, 0x07, 0x60, 0x06, 0x60, 0x06, 0x60, 0x06, 0x60, 0x06, 0xec, 0x37, 0xcc, 0x33, 0x1c, 0x38, 0xf8, 0x1f, 0xe0, 0x07, 0x80, 0x01, 0x80, 0x01, 0xe0, 0x07, 0xf0, 0x0f },
-    { 0xe0, 0x00, 0xf0, 0x01, 0x18, 0x03, 0x58, 0x7b, 0x58, 0x03, 0x58, 0x03, 0x58, 0x7b, 0x58, 0x03, 0x58, 0x03, 0x4c, 0x66, 0xe6, 0x0c, 0xf6, 0x0d, 0xe6, 0x6c, 0x0c, 0x06, 0xf8, 0x03, 0xf0, 0x01 },
-    { 0x00, 0x00, 0x00, 0x06, 0x00, 0x0d, 0x00, 0x0c, 0xfc, 0x0f, 0xfc, 0x07, 0x00, 0x00, 0xfc, 0x0f, 0xfc, 0x3f, 0x00, 0x30, 0xfc, 0x34, 0xfc, 0x1d, 0x80, 0x01, 0x90, 0x01, 0xe0, 0x00, 0x00, 0x00 },
-    { 0x00, 0x00, 0xfc, 0x3f, 0xfe, 0x7f, 0x06, 0x60, 0xf6, 0x6f, 0xf6, 0x6f, 0x06, 0x67, 0x86, 0x63, 0xc6, 0x61, 0xe6, 0x60, 0xf6, 0x6f, 0xf6, 0x6f, 0x06, 0x60, 0xfe, 0x7f, 0xfc, 0x3f, 0x00, 0x00 }
-};
-
-
-
-
 
 void loop() {
-  static byte menuSelec = 0;
 
-  u8g2.firstPage();
-    do{
-        //ROW 1:
-        if( menuSelec > 0 )
-        {
-            u8g2.drawXBMP(4, 2,  14, 14, xMenuUp);
-            u8g2.setFont(u8g2_font_haxrcorp4089_tr);
-            u8g2.setCursor(22, 13);
-            u8g2.print(txtMENU[menuSelec-1]);
+
+
+  nav.doInput();
+  // digitalWrite(LEDPIN, ledCtrl);
+  if (nav.changed(0)) {//only draw if menu changed for gfx device
+    //change checking leaves more time for other tasks
+    u8g2.firstPage();
+    do nav.doOutput(); while(u8g2.nextPage());
+  }
+
+  
+  enc.tick();
+
+    if (enc.turn()) {
+        if(enc.dir()==1){
+            nav.doNav(upCmd);
         }else{
-            u8g2.drawXBMP(4, 2,  14, 14, xMenuUp);
-            u8g2.setFont(u8g2_font_haxrcorp4089_tr);
-            u8g2.setCursor(22, 13);
-            u8g2.print(txtMENU[itemsMENU-1]);
+            nav.doNav(downCmd);
         }
- 
- 
-        //ROW 2:
-        u8g2.setFont(u8g2_font_nokiafc22_tu);
-        u8g2.drawBox(0, 18, 128, 27);
- 
-        u8g2.setColorIndex(0);
-        u8g2.drawXBMP(3, 24, 16, 16, xbmMENU[menuSelec]);
- 
-        if( strlen(dscMENU[menuSelec]) == 0 ){
-            u8g2.drawStr(22, 35, txtMENU[menuSelec]);
-        }else{
-            u8g2.drawStr(22, 30, txtMENU[menuSelec]);
-            u8g2.setFont(u8g2_font_micro_mr);
-            u8g2.drawStr(22, 40, dscMENU[menuSelec]);
-        }
-        u8g2.setColorIndex(1);
- 
- 
-        //ROW 3:
-        if( menuSelec < itemsMENU-1 )
-        {
-            u8g2.drawXBMP(4, 48, 14, 14, xMenuDown);
-            u8g2.setFont(u8g2_font_haxrcorp4089_tr);
-            u8g2.setCursor(22, 58);
-            u8g2.print(txtMENU[menuSelec+1]);
-        }else{
-            u8g2.drawXBMP(4, 48, 14, 14, xMenuDown);
-            u8g2.setFont(u8g2_font_haxrcorp4089_tr);
-            u8g2.setCursor(22, 58);
-            u8g2.print(txtMENU[0]);
-        }
- 
- 
-        u8g2.drawRFrame(0, 0, 128, 64, 5);
     }
-    while( u8g2.nextPage() );
-    ecB.tick();
-    if (ecB.left()) menuSelec--;
-    if (ecB.right()) menuSelec++;
+    if (enc.press()) {
+        nav.doNav(enterCmd);
+    }
+    if(enc.hold()){
+        nav.doNav(escCmd);
+    }
 
-    if (menuSelec == 255){
-      menuSelec = itemsMENU-1;
-    }else if(menuSelec > itemsMENU-1){
-      menuSelec = 0;
-      }
-
-
+ 
 }
-
